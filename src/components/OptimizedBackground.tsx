66c1b431aad8aa7e203ef2f0db9912e878f43a89
DanielP0107 <163358448+DanielP0107@users.noreply.github.com>
2026-03-31 19:52:09 -0300

    Create OptimizedBackground component for optimized background images

---
+++ src/components/OptimizedBackground.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+
+const OptimizedBackground: React.FC<{ imageUrl: string }> = ({ imageUrl, children }) => {
+    const [isImageLoaded, setImageLoaded] = useState(false);
+
+    useEffect(() => {
+        const img = new Image();
+        img.src = imageUrl;
+        img.onload = () => setImageLoaded(true);
+    }, [imageUrl]);
+
+    return (
+        <div style={{
+            backgroundImage: isImageLoaded ? `url(${imageUrl})` : 'none',
+            transition: 'background-image 0.5s ease-in-out',
+            height: '100%',
+            width: '100%',
+            position: 'relative'
+        }}>
+            {children}
+        </div>
+    );
+};
+
+export default OptimizedBackground;
